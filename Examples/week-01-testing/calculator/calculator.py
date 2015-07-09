0a76adf3b5c6f337e5d3bb680f070f7d2fd8fa78
MattsFace <spahmatthew@gmail.com>
2015-07-09 15:59:01 -0700

    blah blah

--- Examples/week-01-testing/calculator/calculator.py
+++ Examples/week-01-testing/calculator/calculator.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+oy#!/usr/bin/env python
 
 """calculator
 
--- Examples/week-01-testing/calculator/calculator_test.py
+++ Examples/week-01-testing/calculator/calculator_test.py
@@ -2,8 +2,8 @@ import unittest
 
 import calculator_functions as calc
 
-class TestCalculatorFunctions(unittest.TestCase):
 
+class TestCalculatorFunctions(unittest.TestCase):
     def setUp(self):
         self.x = 2
         self.y = 3
@@ -11,14 +11,27 @@ class TestCalculatorFunctions(unittest.TestCase):
     def test_add(self):
         self.assertEqual(calc.add(self.x, self.y), 5)
 
+    def test_add_negative(self):
+        pass
+
     def test_subtract(self):
         self.assertEqual(calc.subtract(self.x, self.y), -1)
 
     def test_multiply(self):
         self.assertEqual(calc.multiply(self.x, self.y), 6)
 
+    def test_multiply_by_negative(self):
+        self.x = -2
+        self.y = -3
+        self.assertEqual(calc.multiply(self.x, self.y), 6)
+
     def test_divide(self):
-        self.assertEqual(calc.divide(self.x, self.y), )
+        self.assertEqual(calc.divide(self.x, self.y), 0)
+
+    def test_divde_by_zero(self):
+        self.x = 2
+        self.y = 0
+        self.assertRaises(ZeroDivisionError, calc.divide(self.x, self.y))
 
 
 
